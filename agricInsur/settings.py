36a6c75246bd43741d19b08891c38f94e9d7a080
unknown <fabulouschase1@gmail.com>
2026-03-24 13:56:54 -0700

    Add dj-database-url and specify Python 3.11

--- agricInsur/settings.py
+++ agricInsur/settings.py
@@ -86,18 +86,12 @@ WSGI_APPLICATION = 'agricInsur.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # Database
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL and DATABASE_URL.strip():
-    DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
